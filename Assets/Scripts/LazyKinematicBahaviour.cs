84482ee22bd701d90bf4f181be1450f7b8eed494
isgustavo <hi.gustavo.oliveira@gmail.com>
2018-11-18 16:15:05 -0200

    Update enviroment and add wooden box

---
+++ Assets/Scripts/LazyKinematicBahaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class LazyKinematicBahaviour : MonoBehaviour {
+
+    [SerializeField]
+    private float lazyKinematicTime = .5f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Invoke("SetKinematic", lazyKinematicTime);
+        }
+    }
+
+    private void SetKinematic()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.isKinematic = true;
+    }
+}
